aad9eb6fb047c04845d490e16eb969e3304693bb
Anatolij Dejneko <anatoliideineko@gmail.com>
2021-09-09 01:53:02 +0200

    bug fix

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import LogoStyled from "./LogoStyled";
 
 const Logo = () => {
   return (
     <LogoStyled>
-      <img className="logoImg" src={logo} alt="logo" />
+      <Link className="logoLink" to="/" type="button">
+        <img className="logoImg" src={logo} alt="logo" />
+      </Link>
     </LogoStyled>
   );
 };
--- src/components/logo/LogoStyled.js
+++ src/components/logo/LogoStyled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const LogoStyled = styled.div`
   display: flex;
   align-items: center;
-  cursor: pointer;
 
   .logoImg {
     width: 60px;
